70c80ec87148e3952b77a0aa2117a08dfdd28532
Acer <mammoth@Acers-MacBook-Pro.local>
2020-02-24 11:26:58 +0400

    Created Landmarks project

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Acer on 24/02/2020.
+//  Copyright © 2020 Acer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
